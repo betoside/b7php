a3ea13f50178327cf6c160362285f6d4faa01c7e
Betoside <betoside@gmail.com>
2021-05-03 21:35:38 -0300

    atualizando 02/05 #3

---
+++ mod08-mvc/01-Introducao.php
@@ -0,0 +1,62 @@
+<h4>
+#Módulo 8: Padrão MVC (Model, View, Controller)
+</h4>
+<h2>
+#1: Introdução ao MVC
+</h2>
+
+<pre>
+M -> MODEL
+V -> VIEW
+C -> CONTROLLER
+
+
+- o user requisita a url site.com.br/sobre
+- o mvc vai verificar se esta ROTA esta estabelecida
+- o mvc vai mandar a requisiçao para o CONTROLLER da rota
+-- CARREGAR UM VIEW
+-- CHAMAR UM MODEL 
+    é uma class diretamente relacionada com uma table 
+    table:usuario entao model Usuario
+    pega informacoes e manda para o view
+
+CONTROLLER
+- MODEL
+- VIEW
+
+
+
+
+
+
+--------------------===--------------------
+<strong>#1: Introdução ao MVC</strong>
+#2: MVC: Instalação
+#3: MVC: Rotas
+#4: MVC: Rotas Dinâmicas
+#5: MVC: View Dinâmico
+#6: CRUD em MVC: Create (1/2)
+#7: CRUD em MVC: Create (2/2)
+#8: CRUD em MVC: Read
+#9: CRUD em MVC: Update
+#10: CRUD em MVC: Delete
+#11: CRUD em MVC: Arquivos CSS, Imagens, etc
+
+<strong>Folder:</strong> mod-08-mvc
+
+<a href="01-Introducao.php"><strong>01-Introducao.php</strong></a>
+02-Instalacao.php
+    <a href="mvc/public">localhost:8888/B7Web-PHP/mod08-mvc/mvc/public</a>
+    <a href="https://github.com/suporteb7web/mvc" target="_blank">https://github.com/suporteb7web/mvc</a>
+03-Rotas.php
+04-Rotas-Dinamicas.php
+05-View-Dinamico.php
+06-CRUD-Create-1-2.php
+07-CRUD-Create-2-2.php
+08-CRUD-Read.php
+09-CRUD-Update.php
+10-CRUD-Delete.php
+11-CRUD-Arquivos.php
+</pre>
+
+
---
+++ mod08-mvc/02-Instalacao.php
@@ -0,0 +1,7 @@
+<h4>
+#Módulo 8: Padrão MVC (Model, View, Controller)
+</h4>
+<h2>
+#2: MVC: Instalação
+</h2>
+
